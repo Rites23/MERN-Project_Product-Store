2d1a4e8a4bf635db77bcf7b964892fb79c973025
RITESH <ritesh.balu@gmail.com>
2025-04-22 12:46:40 +0200

    Fix: Separate Vite build output and public directories

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		proxy: {
-			"/api": {
-				target: "http://localhost:5000",
-			},
-		},
-	},
-	build: {
-		outDir: "public", // Set the output directory to 'public'
-	},
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+      },
+    },
+  },
+  build: {
+    outDir: 'dist', 
+    publicDir: 'public', 
+  },
 });
